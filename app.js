bcda3a8266d501b0fa4ed7473f447e8025cdc965
Azim <tamboliazim75@gmail.com>
2023-04-09 01:25:54 -0400

    a9 Mongo fa22

--- app.js
+++ app.js
@@ -4,10 +4,16 @@ import UserController from "./controllers/users/users-controller.js";
 import TuitsController
     from "./controllers/tuits/tuits-controller.js";
 import cors from 'cors'
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+    || 'mongodb://127.0.0.1:27017/tuiter';
+mongoose.connect(CONNECTION_STRING);
+import mongoose from "mongoose";
 
 const app = express();
 app.use(cors())
 app.use(express.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017/tuiter');
 
 helloController(app);
 UserController(app);
